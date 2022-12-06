afcf7c67942ddf553e06651251522240bd65b054
danielpdaniel <artofjquinn@gmail.com>
2022-12-06 18:01:22 -0500

    commit tree 093eb9eea548dce7e6ed0bd8b355494cbfe96038

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+  attr_accessor :kind
+
+def initialize l1, l2, l3
+  lengths = [l1, l2, l3]
+
+    if l1 <= 0
+      raise TriangleError
+    elsif l2 <=0
+      raise TriangleError
+    elsif l3 <= 0
+      raise TriangleError
+    elsif l1 + l2 <= l3
+      raise TriangleError
+    elsif l2 + l3 <= l1
+      raise TriangleError
+    elsif l3 + l1 <= l2
+      raise TriangleError
+    elsif lengths.uniq.length == 1
+        @kind = :equilateral
+    elsif lengths.uniq.length == 2
+         @kind = :isosceles
+    elsif lengths.uniq.length == 3
+        @kind = :scalene
+    end   
+end
+
+class TriangleError < StandardError
+end
+
 end
